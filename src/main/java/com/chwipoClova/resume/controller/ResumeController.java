8375eb456237a4746052db5851dbd41177c9436f
황호윤 <hoyoon9227@gmail.com>
2023-12-11 12:01:55 +0900

    불필요한 소스 제거

--- src/main/java/com/chwipoClova/resume/controller/ResumeController.java
+++ src/main/java/com/chwipoClova/resume/controller/ResumeController.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.service.annotation.DeleteExchange;
 
 import java.util.List;
 
