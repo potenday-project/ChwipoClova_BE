11a44040f66c8c90cab70117d73ba0f9d2469a25
황호윤 <hoyoon9227@gmail.com>
2024-01-10 00:53:57 +0900

    cors 설정 변경

---
+++ src/main/java/com/chwipoClova/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.chwipoClova.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origins}")
+    private String[] origins;
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,POST,PATCH,PUT,DELETE,OPTIONS";
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(origins) // 허용할 출처
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowCredentials(true); // 허용할 HTTP method
+    }
+}
