cbd6538579d755016a502179f22ad32d1165b08a
황호윤 <hoyoon9227@gmail.com>
2024-01-21 02:05:18 +0900

    domain 값 변경

--- src/main/java/com/chwipoClova/common/utils/JwtUtil.java
+++ src/main/java/com/chwipoClova/common/utils/JwtUtil.java
@@ -41,7 +41,7 @@ public class JwtUtil {
 
     public static final String ACCESS_TOKEN = "accessToken";
 
-    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String REFRESH_TOKEN = "refresh_token";
 
     public static final String AUTHORIZATION = "Authorization";
 
