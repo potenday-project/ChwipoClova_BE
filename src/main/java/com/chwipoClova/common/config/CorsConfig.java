93a2d64e147b58cadbb629fb0931276dad241616
황호윤 <hoyoon9227@gmail.com>
2024-01-23 16:01:49 +0900

    ExposedHeaders header 수정

--- src/main/java/com/chwipoClova/common/config/CorsConfig.java
+++ src/main/java/com/chwipoClova/common/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.chwipoClova.common.config;
 
+import com.chwipoClova.common.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,7 +26,7 @@ public class CorsConfig {
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setExposedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("cache-control", "content-length", "content-type", "expires", "pragma", HttpHeaders.SET_COOKIE, JwtUtil.ACCESS_TOKEN));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
